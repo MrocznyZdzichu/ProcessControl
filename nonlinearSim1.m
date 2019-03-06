deb7f567f418128a2b7a22a0de1b42983e303db5
Max <276786@pw.edu.pl>
2019-03-06 23:50:28 +0100

    Added delays of Fc and T_out

--- nonlinearSim1.m
+++ nonlinearSim1.m
@@ -1,14 +1,16 @@
-function [] = nonlinearSim1(u, op_Fc, op_Fh, op_X, stop_time, op_tauc)
-    t_Fc = linspace(0, stop_time, stop_time);                    %time vectors for time-dependent control
-    t_Fh = linspace(0, stop_time, stop_time);
-      
+function [] = nonlinearSim1(u, op_X, stop_time, op_tauc, op_tau)
+    t_ = linspace(0, stop_time, stop_time);                    %time vectors for time-dependent control   
     tspan = linspace(0, stop_time, stop_time);
+    
     delayed_Fc = u(2, 1:op_tauc);
     delayed_Fc = [delayed_Fc, u(2, 1:end-op_tauc)];
-    [t, X] = ode23(@(t, X)derivative_X(t, X, t_Fc, delayed_Fc, t_Fh, u(1, :)), tspan, op_X);
+    [t, X] = ode23(@(t, X)derivative_X(t, X, t_, delayed_Fc, u(1, :)), tspan, op_X);
     
-    ts_Fc = interp1(t_Fc, u(2, :), t);
-    ts_Fh = interp1(t_Fh, u(1, :), t);
+    T_out = X(1, 2)*ones(op_tau, 1);            %T_out is T delayed by op_tau
+    T_out = [T_out; X(1:end-op_tau, 2)];
+    
+    ts_Fc = interp1(t_, u(2, :), t);
+    ts_Fh = interp1(t_, u(1, :), t);
     
     figure(1)
     subplot(2, 1, 1);
@@ -20,7 +22,7 @@ function [] = nonlinearSim1(u, op_Fc, op_Fh, op_X, stop_time, op_tauc)
     
     figure(2)
     subplot(2, 1, 1);
-    plot(t, X(:, 2))
+    plot(t, T_out)
     subplot(2, 1, 2);
     plot(t, ts_Fc)
     hold on
--- pr2PlonskiRusakZawrotny.m
+++ pr2PlonskiRusakZawrotny.m
@@ -1,4 +1,12 @@
 initialisation;
 %simulateInOperatingPoint;          %non-linear simulation near operating point
-u = CVstepChange(-15, 10, 50000, 20000, 100000, op_Fh, op_Fc);
-nonlinearSim1(u, op_Fc, op_Fh, op_X, 100000, op_tauc);
+
+step_Fh = -15;
+step_Fc = 10;
+stepTime_Fh = 5e4;
+stepTime_Fc = 2e4;
+timeRange = 1e5;
+Fd_amplitude = 4;
+
+u = CVstepChange(step_Fh, step_Fc, stepTime_Fh, stepTime_Fc, timeRange, op_Fh, op_Fc);
+nonlinearSim1(u, op_X, 100000, op_tauc, op_tau);
